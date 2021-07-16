b9b6d8a25df2529c2e751c9cbcb0049218ec88cd
Juliette Curran <juliettecurran@hotmail.com>
2021-07-17 00:29:23 +0100

    Moved commentSection to its own component

---
+++ src/Components/ReviewByID/CommentSection.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import Expandable from '../Shared/Expandable';
+import Voter from '../Shared/Voter';
+import { getComments } from '../../utils/api';
+import { useParams } from 'react-router-dom';
+
+const CommentSection = () => {
+  const { review_id } = useParams();
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(review_id).then((commentsFromApi) => {
+      setComments(commentsFromApi);
+    });
+  }, [review_id]);
+
+  return (
+    <Expandable>
+      <div className='comments'>
+        <ul className='commentsList'>
+          <h3>Comments</h3>
+          <hr className='commentDivider'></hr>
+          <section className='commentArea'>
+            <Form>
+              <Form.Group as={Row} className='mb-3' controlId='review'>
+                <Col sm={12} md={12}>
+                  <Form.Control as='textarea' placeholder='Your comment' />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} className='mb-3'>
+                <Button className='submitBtn' type='submit'>
+                  Submit
+                </Button>
+              </Form.Group>
+            </Form>
+          </section>
+        </ul>
+        {comments.map((comment) => {
+          return (
+            <li key={comment.comment_id} className='singleComment'>
+              <p>
+                <Badge className='userBadge' pill bg='warning' text='dark'>
+                  {comment.author}
+                </Badge>{' '}
+                - {comment.body}
+              </p>
+              <p className='created_at'> posted at {comment.created_at}</p>
+
+              <Voter
+                vote_type={'comments'}
+                id={comment.comment_id}
+                votes={comment.votes}
+              />
+            </li>
+          );
+        })}
+      </div>
+    </Expandable>
+  );
+};
+
+export default CommentSection;
--- src/Components/ReviewByID/SingleReview.jsx
+++ src/Components/ReviewByID/SingleReview.jsx
@@ -2,20 +2,13 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Badge from 'react-bootstrap/Badge';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { getComments, postComment } from '../../utils/api';
-import Expandable from '../Shared/Expandable';
 import Voter from '../Shared/Voter';
-
+import CommentSection from './CommentSection';
 import '././singleReview.css';
 
 const SingleReview = () => {
   const [singleReview, setSingleReview] = useState({});
   const { review_id } = useParams();
-  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     return axios
@@ -23,12 +16,6 @@ const SingleReview = () => {
       .then((response) => {
         setSingleReview(response.data.review);
       });
-  }, [review_id]);
-
-  useEffect(() => {
-    getComments(review_id).then((commentsFromApi) => {
-      setComments(commentsFromApi);
-    });
   }, [review_id]);
 
   return (
@@ -56,48 +43,7 @@ const SingleReview = () => {
         />
       </div>
       <hr className='commentDivider'></hr>
-      <Expandable>
-        <div className='comments'>
-          <ul className='commentsList'>
-            <h3>Comments</h3>
-            <hr className='commentDivider'></hr>
-            <section className='commentArea'>
-              <Form>
-                <Form.Group as={Row} className='mb-3' controlId='review'>
-                  <Col sm={12} md={12}>
-                    <Form.Control as='textarea' placeholder='Your comment' />
-                  </Col>
-                </Form.Group>
-
-                <Form.Group as={Row} className='mb-3'>
-                  <Button className='submitBtn' type='submit'>
-                    Submit
-                  </Button>
-                </Form.Group>
-              </Form>
-            </section>
-          </ul>
-          {comments.map((comment) => {
-            return (
-              <li key={comment.comment_id} className='singleComment'>
-                <p>
-                  <Badge className='userBadge' pill bg='warning' text='dark'>
-                    {comment.author}
-                  </Badge>{' '}
-                  - {comment.body}
-                </p>
-                <p className='created_at'> posted at {comment.created_at}</p>
-
-                <Voter
-                  vote_type={'comments'}
-                  id={comment.comment_id}
-                  votes={comment.votes}
-                />
-              </li>
-            );
-          })}
-        </div>
-      </Expandable>
+      <CommentSection />
     </main>
   );
 };
